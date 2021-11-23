a7e93813d948e2bd861ebdf5b43d3a5d3cd8e417
Jamie <lijamie19828@gmail.com>
2021-11-23 03:36:30 +0000

    total submissions by cohort

---
+++ 3_queries_group_by/total_submissions.sql
@@ -0,0 +1,10 @@
+SELECT 
+  cohorts.name AS cohort,
+  COUNT(*) AS total_submissions
+FROM cohorts
+INNER JOIN students
+  ON cohorts.id = students.cohort_id
+INNER JOIN assignment_submissions
+  ON students.id = assignment_submissions.student_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
